3528945b6d28534c4ec5e3d6777362863616898e
AnghanPujan <poojan.anghan@exhibytesolution.com>
2025-08-13 15:33:03 +0530

    Student model registred to adminsite

--- core_django/student/admin.py
+++ core_django/student/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from student.models import student 
 
-admin.site.register(student)
+# admin.site.register(student)
+
+@admin.register(student)   # Regestration of the tabel in admin panel
+class StudentAdmin(admin.ModelAdmin): # setting the view of the tabel in admin panel 
+    list_display = ('name', 'age', 'roll_no') # parameters which will shown to admin
